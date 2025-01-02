d86f9bb4630073380a1373db86af5240a9d9774d
Siddhartha Tiwari <104570057+MetaHat@users.noreply.github.com>
2025-01-02 20:17:39 +0530

    Update header.php

--- src/component/header.php
+++ src/component/header.php
@@ -165,7 +165,7 @@
                     <div class="item"><a href="<?= $discord ?>" target="_blank" class="zr-social-button dc-btn"><i class="fab fa-discord"></i></a></div>
                     <div class="item"><a href="<?= $facebook ?>" target="_blank" class="zr-social-button dc-btn"><i class="fab fa-facebook"></i></a></div>
                     <div class="item"><a href="<?= $twitter ?>" target="_blank" class="zr-social-button dc-btn"><i class="fab fa-twitter"></i></a></div>
-                    <div class="item"><a href="<?= $instagram ?>" target="_blank" class="zr-social-button gh-btn"><i class="fab fa-instagram"></i></a></div>
+                    <div class="item"><a href="<?= $github ?>" target="_blank" class="zr-social-button gh-btn"><i class="fab fa-github"></i></a></div>
                 </div>
                 <div class="clearfix"></div>
             </div>
