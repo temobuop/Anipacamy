799db91ef01eca5f9ccfd19e0fbc9827178042ec
Raisul Islam <118072581+PacaHat@users.noreply.github.com>
2025-01-03 00:40:37 +0600

    added recommended section

--- src/user/watchlist.php
+++ src/user/watchlist.php
@@ -300,7 +300,87 @@ $total_pages = ceil($total_site_items / $items_per_page);
                     </div>
                 </div>
             </div>
-            
+            <section class="block_area block_area_category lazy-component mt-4" data-component="category">
+                        <div class="block_area-header">
+
+                            <div class="float-left bah-heading mr-4">
+                                <h2 class="cat-heading">Wann add more anime?</h2>
+                            </div>
+                            <div class="float-right viewmore">
+                                <a class="btn" href="/anime/top-airing">View more<i
+                                        class="fas fa-angle-right ml-2"></i></a>
+                            </div>
+                            <div class="clearfix"></div>
+                        </div>
+
+                        <div class="tab-content">
+                            <div class="block_area-content block_area-list film_list film_list-grid film_list-wfeature ">
+                                <div class="film_list-wrap">
+                                    
+                                    <?php
+                                    // Fetch JSON data
+                                    $json = file_get_contents("$zpi/top-airing");
+                                    $json = json_decode($json, true);
+
+                                    // Check if 'results' and 'data' exist
+                                    if (isset($json['results']['data']) && is_array($json['results']['data'])) {
+                                        $animeList = array_slice($json['results']['data'], 0, 12);
+                                        foreach ($animeList as $anime) { ?>
+                                            <div class="flw-item">
+                                                <div class="film-poster">
+                                                    <!-- Age Indicator -->
+                                                    <?php if ($anime['adultContent']) { ?>
+                                                        <div class="tick ltr" style="position: absolute; top: 10px; left: 10px;">
+                                                            <div class="tick-item tick-age amp-algn">18+</div>
+                                                        </div>
+                                                    <?php } ?>
+                                                    <!-- Sub and Dub Counts -->
+                                                    <div class="tick ltr" style="position: absolute; bottom: 10px; left: 10px;">
+                                                        <div class="tick-item tick-sub amp-algn" style="text-align: left;">
+                                                            <i class="fas fa-closed-captioning"></i>&nbsp;<?= $anime['tvInfo']['sub'] ?>
+                                                        </div>
+                                                        <?php if(!empty($anime['tvInfo']['dub'])): ?>
+                                                        <div class="tick-item tick-dub amp-algn" style="text-align: left;">
+                                                            <i class="fas fa-microphone"></i>&nbsp;<?= $anime['tvInfo']['dub'] ?>
+                                                        </div>
+                                                        <?php endif; ?>
+                                                    </div>
+                                                    <!-- Anime Poster -->
+                                                    <img class="film-poster-img lazyload"
+                                                        data-src="<?= $anime['poster'] ?>"
+                                                        src="<?= $anime['poster']?>"
+                                                        alt="<?= $anime['title'] ?>">
+                                                    <a class="film-poster-ahref"
+                                                        href="/details/<?= $anime['id'] ?>"
+                                                        title="<?= $anime['title'] ?>"
+                                                        data-jname="<?= $anime['title'] ?>"><i class="fas fa-play"></i></a>
+                                                </div>
+                                                <div class="film-detail">
+                                                    <h3 class="film-name">
+                                                        <a href="/details/<?= $anime['id'] ?>"
+                                                            title="<?= $anime['title'] ?>"
+                                                            data-jname="<?= $anime['title'] ?>"><?= $anime['title'] ?></a>
+                                                    </h3>
+                                                    <div class="fd-infor">
+                                                       
+                                                        <span class="fdi-item"><?= $anime['tvInfo']['showType'] ?></span>
+                                                        <span class="dot"></span>
+                                                        <span class="fdi-item"><?= $anime['tvInfo']['duration'] ?></span>
+                                                    </div>
+                                                </div>
+                                                <div class="clearfix"></div>
+                                            </div>
+                                    <?php }
+                                    } else {
+                                        echo "<p>No anime data available or invalid structure.</p>";
+                                    } ?>
+
+                                </div>
+                                <div class="clearfix"></div>
+        
+                            </div>
+                        </div>
+                    </section>
           </div>
         </div>
         <div class="clearfix"></div>
