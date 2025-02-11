e36214c938bd2e8ff387e893d71d177275b22e3e
Raisul Islam <118072581+PacaHat@users.noreply.github.com>
2025-02-12 03:02:48 +0600

    n

--- _config.php
+++ _config.php
@@ -1,10 +1,10 @@
 <?php 
 
-$conn = new mysqli("HOSTNAME", "USERNAME", "PASSWORD", "DATABASE");
+$conn = new mysqli("localhost", "anipacat_anipaca", "iJeTUMxpeR#R", "anipacat_anime");
 
 if ($conn->connect_error) {
     error_log("Database connection failed: " . $conn->connect_error);
-    
+    die("Connection failed: " . $conn->connect_error);
 }
 
 $websiteTitle = "AniPaca";
@@ -16,16 +16,13 @@ $version = "0.1";
 
 $discord = "https://dcd.gg/anipaca";
 $github = "https://github.com/PacaHat";
-$telegram = "https://t.me/anipaca";
-$instagram = "https://www.instagram.com/pxr15_";
+$twitter = "https://x.com/PacaHat";
 
 // all the api you need
 
-$api = "https://your-hosted-api.com/api/v2/hianime"; //https://github.com/ghoshRitesh12/aniwatch-api Also use this in src/component/qtip.php
-$zpi = "https://your-hosted-api.com/api"; //https://github.com/itzzzme/anime-api
-$proxy = "https://your-hosted-proxy.com/cors?url="; //https://github.com/shashstormer/m3u8_proxy-cors
+$api = "https://hi.anime.org.im/api/v2/hianime"; //https://github.com/ghoshRitesh12/aniwatch-api Also use this in src/component/qtip.php
+$zpi = "https://zen.anime.org.im/api"; //https://github.com/itzzzme/anime-api
+$proxy = "your-hosted-proxy.com/cors?url="; //https://github.com/shashstormer/m3u8_proxy-cors
 
 
 $banner = $websiteUrl . "/public/images/banner.png";
-
-    
