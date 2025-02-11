ab37e7d565f13e0755c83fd3970a24c3efd0c6cd
Raisul Islam <118072581+PacaHat@users.noreply.github.com>
2025-02-12 02:05:22 +0600

    Update _config.php

--- _config.php
+++ _config.php
@@ -4,7 +4,7 @@ $conn = new mysqli("HOSTNAME", "USERNAME", "PASSWORD", "DATABASE");
 
 if ($conn->connect_error) {
     error_log("Database connection failed: " . $conn->connect_error);
-    die("Connection failed: " . $conn->connect_error);
+    
 }
 
 $websiteTitle = "AniPaca";
