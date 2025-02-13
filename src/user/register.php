a06dfb9b54079f476d83a21bdb8a6daffe755546
Raisul Islam <118072581+PacaHat@users.noreply.github.com>
2025-02-13 14:22:29 +0600

    Update register.php

--- src/user/register.php
+++ src/user/register.php
@@ -210,7 +210,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <body data-page="page_register">
     <div id="sidebar_menu_bg"></div>
     <div id="wrapper" data-page="page_home">
-    <?php include '/src/component/header.php'; ?>
+    <?php include 'src/component/header.php'; ?>
         <div class="clearfix"></div>
         <div id="main-wrapper" class="layout-page layout-page-404">
             <div class="container">
