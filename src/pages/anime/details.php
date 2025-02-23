cb6d34bce85802356b15da53b8e328902fdb0e54
ariaubrot <ariaputrapratamaaa@gmail.com>
2025-02-23 13:08:10 +0700

    Update details.php
    fix footer error

--- src/pages/anime/details.php
+++ src/pages/anime/details.php
@@ -530,7 +530,7 @@ $characterDataJson = json_encode($characterData, JSON_PRETTY_PRINT);
             <div class="clearfix"></div>
         </div>
     </div>
-    <?php include('/src/component/footer.php'); ?>
+    <?php include('src/component/footer.php'); ?>
     <div id="mask-overlay"></div>
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
