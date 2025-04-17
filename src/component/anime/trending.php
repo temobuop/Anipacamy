4ed3ebc24f9c3e4323f5002bb8e3bdfe81d64702
Raisul Islam <118072581+PacaHat@users.noreply.github.com>
2025-04-17 19:35:51 +0600

    trending loading glitch fixed

--- src/component/anime/trending.php
+++ src/component/anime/trending.php
@@ -1,62 +1,41 @@
 <div id="anime-trending">
-    <div class="container">
-        <section class="block_area block_area_trending">
-            <div class="block_area-header">
-                <div class="bah-heading">
-                    <h2 class="cat-heading">Trending Anime</h2>
+        <div class="container">
+            <section class="block_area block_area_trending">
+                <div class="block_area-header">
+                    <div class="bah-heading">
+                        <h2 class="cat-heading">Trending</h2>
+                    </div>
+                    <div class="clearfix"></div>
                 </div>
-                <div class="clearfix"></div>
-            </div>
-            <div class="block_area-content">
-                <div class="trending-list" id="trending-home">
-                    <div class="trending-container-anime swiper-container">
-                        <div class="swiper-wrapper">
-                            <?php if (!empty($data['trending'])): ?>
-                                <?php foreach ($data['trending'] as $index => $anime): ?>
-                                    <div class="swiper-slide item-qtip" data-id="<?= htmlspecialchars($anime['id'], ENT_QUOTES, 'UTF-8') ?>">
+                <div class="block_area-content">
+                    <div class="trending-list" id="trending-home" style="">
+                        <div class="swiper-container swiper-container-initialized swiper-container-horizontal">
+                            <div class="swiper-wrapper" style="transition-duration: 0ms; transform: translate3d(0px, 0px, 0px);">
+                                <?php foreach ($data['trending'] as $item): ?>
+                                    <div class="swiper-slide item-qtip" data-id="<?= htmlspecialchars($item['data_id']) ?>" style="width: 209px; margin-right: 15px;">
                                         <div class="item">
                                             <div class="number">
-                                                <span><?= str_pad($index + 1, 2, '0', STR_PAD_LEFT) ?></span>
-                                                <div class="film-title dynamic-name" data-jname="<?= htmlspecialchars($anime['japanese_title'], ENT_QUOTES, 'UTF-8') ?>">
-                                                    <?= htmlspecialchars($anime['title'], ENT_QUOTES, 'UTF-8') ?>
-                                                </div>
+                                                <span><?= htmlspecialchars($item['number']) ?></span>
+                                            <div class="film-title dynamic-name" data-title="<?= htmlspecialchars($item['title']) ?>" data-jname="<?= htmlspecialchars($item['japanese_title']) ?>">
+                                                <?= htmlspecialchars($item['title']) ?>
                                             </div>
-                                            <a href="/details/<?= htmlspecialchars($anime['id'], ENT_QUOTES, 'UTF-8') ?>" class="film-poster">
-                                            <img src="<?= htmlspecialchars($anime['poster'], ENT_QUOTES, 'UTF-8') ?>" class="film-poster-img lazyload" title="<?= htmlspecialchars($anime['title'], ENT_QUOTES, 'UTF-8') ?>" alt="<?= htmlspecialchars($anime['title'], ENT_QUOTES, 'UTF-8') ?>">
-                                            </a>
-                                            <div class="clearfix"></div>
                                         </div>
+                                        <a href="/<?= htmlspecialchars($item['id']) ?>" class="film-poster">
+                                            <img data-src="<?= htmlspecialchars($item['poster']) ?>" class="film-poster-img lazyloaded" alt="<?= htmlspecialchars($item['title']) ?>" src="<?= htmlspecialchars($item['poster']) ?>">
+                                        </a>
+                                        <div class="clearfix"></div>
                                     </div>
-                                <?php endforeach; ?>
-                            <?php else: ?>
-                                <p>No trending animes available at the moment.</p>
-                            <?php endif; ?>
+                                </div>
+                            <?php endforeach; ?>
                         </div>
                         <div class="clearfix"></div>
+                        <span class="swiper-notification" aria-live="assertive" aria-atomic="true"></span></div>
+                        <div class="trending-navi">
+                            <div class="navi-next" tabindex="0" role="button" aria-label="Next slide" aria-disabled="false"><i class="fas fa-angle-right"></i></div>
+                            <div class="navi-prev swiper-button-disabled" tabindex="-1" role="button" aria-label="Previous slide" aria-disabled="true"><i class="fas fa-angle-left"></i></div>
                         </div>
-                    <div class="trending-navi" style="display: flex; flex-direction: column; align-items: center;">
-                        <div class="navi-next navi-next-anime" tabindex="0" role="button" aria-label="Next slide" aria-disabled="false"><i class="fas fa-angle-right"></i></div>
-                        <div class="navi-prev swiper-button-disabled navi-prev-anime" tabindex="-1" role="button" aria-label="Previous slide" aria-disabled="true"><i class="fas fa-angle-left"></i></div>
                     </div>
                 </div>
-            </div>
-        </section>
+            </section>
+        </div>
     </div>
-</div>
-
-<script src="<?= $websiteUrl ?>/src/assets/js/swiper-bundle.min.js"></script>
-
-<script>
-    var trendingSlider = new Swiper('.trending-container-anime', {
-        slidesPerView: 3,
-        breakpoints: {
-            768: {
-                slidesPerView: 6
-            }
-        },
-        navigation: {
-            nextEl: '.navi-next-anime',
-            prevEl: '.navi-prev-anime',
-        },
-    });
-</script>
