83e6acbb0d2f744835ad1ed26da52834783a833a
Raisul Islam <118072581+PacaHat@users.noreply.github.com>
2025-05-22 23:10:02 +0600

    Update _config.php
    added some demo

--- _config.php
+++ _config.php
@@ -1,19 +1,20 @@
 <?php 
 
-$conn = new mysqli("HOSTNAME", "USERNAME", "PASSWORD", "DATABASE");
+$conn = new mysqli("HOSTNAME", "USERNAME", "PASSWORD", "DATABASE"); //just like $conn = new mysqli("localhost", "root", "", "anipaca");
+
 
 if ($conn->connect_error) {
     error_log("Database connection failed: " . $conn->connect_error);
-    die("Database connection failed.");
+    echo("Database connection failed.");
 }
 
 $websiteTitle = "AniPaca";
 $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? "https" : "http";
 $websiteUrl = "{$protocol}://{$_SERVER['SERVER_NAME']}";
 $websiteLogo = $websiteUrl . "/public/logo/logo.png";
-$contactEmail = "@gmail.com";
+$contactEmail = "raisulentertainment@gmail.com";
 
-$version = "2.1";
+$version = "1.0.2";
 
 $discord = "https://dcd.gg/anipaca";
 $github = "https://github.com/PacaHat";
@@ -24,8 +25,8 @@ $instagram = "https://www.instagram.com/pxr15_";
 $zpi = "https://your-hosted-api.com/api"; //https://github.com/PacaHat/zen-api
 $proxy = $websiteUrl . "/src/ajax/proxy.php?url=";
 
-//If you want faster loading speed
-//$proxy = "https://your-hosted-proxy.com/api/v1/streamingProxy?url="; //https://github.com/MetaHat/m3u8-streaming-proxy
+//If you want faster loading speed just put // before the first proxy and remove slashes from this one 
+//$proxy = "https://your-hosted-proxy.com/proxy?url="; //https://github.com/PacaHat/shrina-proxy
 
 
 $banner = $websiteUrl . "/public/images/banner.png";
