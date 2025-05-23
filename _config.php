04da17b3f90f90d4da75c1d644f71bf338e57746
temobuop <temobuop@gmail.com>
2025-05-23 18:28:47 +0600

    Update _config.php

--- _config.php
+++ _config.php
@@ -22,7 +22,7 @@ $telegram = "https://t.me/anipaca";
 $instagram = "https://www.instagram.com/pxr15_"; 
 
 // all the api you need
-$zpi = "https://api-gamma-lac-17.vercel.app/"; //https://github.com/PacaHat/zen-api
+$zpi = "https://api-gamma-lac-17.vercel.app/api"; //https://github.com/PacaHat/zen-api
 $proxy = $websiteUrl . "/src/ajax/proxy.php?url=";
 
 //If you want faster loading speed just put // before the first proxy and remove slashes from this one 
